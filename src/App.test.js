2c1a512dffe06da35fad00f64391c1b35e4a034f
hendrism <hendrism@gmail.com>
2025-07-29 15:02:27 -0400

    Fix test and restore tailwind v3

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Stellar Expeditions title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Stellar Expeditions/i);
+  expect(heading).toBeInTheDocument();
 });
